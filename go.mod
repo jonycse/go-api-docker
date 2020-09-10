0d86c0a5ec69ed1afeeb68174934299a0c18c2b6
Jony <jony.kaz2019@gmail.com>
2020-09-10 10:33:40 +0600

    added file watcher

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jonycse/go-api-docker
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/fsnotify/fsnotify v1.4.9
+	github.com/gorilla/mux v1.7.3
+)
--- main.go
+++ main.go
@@ -5,10 +5,28 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/fsnotify/fsnotify"
 )
+
+// watch this data path, notify main if file has changes
+const dataPath = "/home/jony/Developer/jony/go-api-docker/data"
 
 func main() {
 	r := mux.NewRouter()
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		panic(err)
+	}
+	defer watcher.Close()
+
+	go watchData(watcher)
+
+	// watching data path
+	err = watcher.Add(dataPath)
+	if err != nil {
+		panic(err)
+	}
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world")
@@ -16,4 +34,23 @@ func main() {
 
 	fmt.Println("Server listening!")
 	http.ListenAndServe(":8080", r)
+}
+
+func watchData(watcher *fsnotify.Watcher) {
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return
+			}
+			if event.Op&fsnotify.Write == fsnotify.Write {
+				fmt.Println("Modified file: %s", event.Name)
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return
+			}
+			panic(err)
+		}
+	}
 }
